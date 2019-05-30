19a5484b220a06c4f88b265e4b12dd1260857f60
Ian Lin <ian.yian.lin@gmail.com>
2019-05-30 22:16:06 +0800

    upgrade CI (#6)
    * update tox; use pipenv
    
    * update travis

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: BSD License',
     ],
     test_suite='nose.collector',
