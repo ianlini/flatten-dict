ef85fdaf82118b32942c1b92e4b8bdf128bacd00
Ian Lin <ian.yian.lin@gmail.com>
2019-09-13 16:46:41 +0800

    add python 3.8 support to classifiers

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: BSD License',
     ],
     packages=find_packages(),
